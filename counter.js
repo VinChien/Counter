b81737b8bb71966cea89a63cb9f64a273fc09bd1
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-05 11:20:19 +0800

    完成計數器

--- counter.js
+++ counter.js
@@ -6,18 +6,12 @@ $(document).ready(function () {
             // element == this
             var offset = $(this).offset();
             var top = offset.top;
-
             var windowTop = $(window).scrollTop();
-            // console.log("windowTop="+windowTop);
 
             // 取得屬性裡的值
             var number = $(this).attr("data-counter-number");
             var time = $(this).attr("data-counter-time");
             var offset = $(this).attr("data-counter-offset");
-
-            // console.log("數字 = " + number);
-            // console.log("時間 = " + time);
-            // console.log("位移 = " + offset);
 
             if (windowTop > top - offset) {
 
