08c1d2e44d505e69b6a6fe8a92770870cbb18c2c
jianfan0114@gmail.com <jianfan0114@gmail.com>
2020-07-05 11:07:49 +0800

    jQ.attr、jQ.each

--- counter.js
+++ counter.js
@@ -2,27 +2,36 @@ $(document).ready(function () {
 
     $(window).scroll(function () {
 
-        var offset = $(".counter").offset();
-        var top = offset.top;
-        var left = offset.left;
-        // var counterHeight = $(".counter").outerHeight();
-        // console.log("counterHeight="+counterHeight);
-        // console.log("divtop="+top);
+        $("*").each(function (index, element) {
+            // element == this
+            var offset = $(this).offset();
+            var top = offset.top;
 
-        var windowTop = $(window).scrollTop();
-        // console.log("windowTop="+windowTop);
+            var windowTop = $(window).scrollTop();
+            // console.log("windowTop="+windowTop);
 
-        if (windowTop > top ) {
-            
-            $(".counter").animate({
-                num:777
-            },{
-                duration:2000,
-                step:(now)=>{
-                    $(".counter").text(Math.floor(now));
-                }
-            })
-        }
+            // 取得屬性裡的值
+            var number = $(this).attr("data-counter-number");
+            var time = $(this).attr("data-counter-time");
+            var offset = $(this).attr("data-counter-offset");
+
+            // console.log("數字 = " + number);
+            // console.log("時間 = " + time);
+            // console.log("位移 = " + offset);
+
+            if (windowTop > top - offset) {
+
+                $(this).animate({
+                    num: number,
+                }, {
+                    duration: parseInt(time),
+                    step: (now) => {
+                        $(this).text(Math.floor(now));
+                    }
+                })
+            }
+
+        });
 
 
     });
